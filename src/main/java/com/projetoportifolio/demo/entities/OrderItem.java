dbd26db08bdbd0c4b2cb3ad6a6e445f40b7b0a42
Bruno Souto <brunosoutosilva@Hotmail.com>
2023-04-26 12:55:40 -0300

    ajustando relacionamento entre produto pedido e item de pedido

--- src/main/java/com/projetoportifolio/demo/entities/OrderItem.java
+++ src/main/java/com/projetoportifolio/demo/entities/OrderItem.java
@@ -9,8 +9,9 @@ import com.projetoportifolio.demo.entities.pk.OrderItemPK;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+
 @Entity
-@Table(name= "tb_order_item")
+@Table(name = "tb_order_item")
 public class OrderItem implements Serializable {
 
 	private static final long serialVersionUID = 1L;
