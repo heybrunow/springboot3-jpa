eb9f62ceaa5c6a38bfaeb70a3959ddcf94283e99
Bruno Souto <brunosoutosilva@Hotmail.com>
2023-04-26 14:57:56 -0300

    adicionando tratamento de excecao para usuario nao encontrado

--- src/main/java/com/projetoportifolio/demo/services/UserService.java
+++ src/main/java/com/projetoportifolio/demo/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.projetoportifolio.demo.entities.User;
 import com.projetoportifolio.demo.repositories.UserRepository;
+import com.projetoportifolio.demo.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 	
 	public User findbyId(Long id) {
 		Optional<User> user  = userRepository.findById(id);
-		return user.get();
+		return user.orElseThrow(()-> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User user) {
